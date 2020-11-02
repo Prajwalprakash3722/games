df3667de1683922244a7634e6e1f95e84b9af344
Prajwal prakash <71436720+Prajwalprakash3722@users.noreply.github.com>
2020-11-02 20:51:49 +0530

    Create game1.py

---
+++ game1.py
@@ -0,0 +1,26 @@
+import math, random
+def generatenumber():
+  
+
+    digits = "0123456789"
+    number = ""
+
+
+    for i in range(1):
+        number += digits[math.floor(random.random() * 10)]
+
+    return number
+
+
+if __name__ == "__main__":
+
+ guess_count=0
+guess_limit=3
+while guess_count<guess_limit:
+    guess= int(input("Guess: "))
+    guess_count +=1
+    if guess==generatenumber():
+        print("you won")
+        break
+else:
+      print(f"You exceeded your chances, the random number was  {generatenumber()}")
