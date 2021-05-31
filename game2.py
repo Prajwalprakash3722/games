9e08024cdf5376625648e9f51d0d9a5879aa34aa
Prajwal prakash <prajwalprakash3722@gmail.com>
2021-05-31 15:59:14 +0530

    added .lower() closes#1

--- game2.py
+++ game2.py
@@ -6,9 +6,9 @@
 from random import randint, choice
 import sys
 
-v = ["Rock", "Paper", "Scissors"]
+v = ["rock", "paper", "scissors"]
 
-computer_move = choice(v)
+computer_move = choice(v).lower()
 
 player_move = False
 tie = 0
@@ -19,26 +19,26 @@ while not player_move:
 Paper,
 Scissors,
 (Q) to Quit
-""")
+""").lower()
     if player_move == computer_move:
         tie += 1
         print("-->Tie! Total ties:", tie)
-    elif player_move == "Rock":
-        if computer_move == "Paper":
+    elif player_move == "rock":
+        if computer_move == "paper":
             lose += 1
             print("-->You lose! Total loses:", lose)
         else:
             win += 1
             print("-->You win! Total wins:", win)
-    elif player_move == "Paper":
-        if computer_move == "Scissors":
+    elif player_move == "paper":
+        if computer_move == "scissors":
             lose += 1
             print("-->You lose! Total loses:", lose)
         else:
             win += 1
-            print("-->You win!")
-    elif player_move == "Scissors":
-        if computer_move == "Rock":
+            print("-->You win! Total wins:", win)
+    elif player_move == "scissors":
+        if computer_move == "rock":
             lose += 1
             print("-->You lose! Total loses:", lose)
         else:
@@ -50,4 +50,3 @@ Scissors,
         print("-->That's not a valid play. Check your spelling (Case sensitive)")
     player_move = False
     computer_move = v[randint(0, 2)]
-    
