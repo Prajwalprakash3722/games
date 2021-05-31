02b16e318135c92b041a8211a95b4df031d0eb26
Manu S Rao <67941652+MANUSRAO@users.noreply.github.com>
2021-05-31 17:18:42 +0530

    Updated game2.py to accept inputs in any case

--- game2.py
+++ game2.py
@@ -11,37 +11,37 @@ tie = 0
 win = 0
 lose = 0
 while not playing:
-    computer_move = choice(v).lower()
+    computer_move = choice(v)
     player_move = input("""Rock,
 Paper,
 Scissors,
 (Q) to Quit
-""").lower()
+""").capitalize()
     if player_move == computer_move:
         tie += 1
         print("-->Tie! Total ties:", tie)
-    elif player_move == "Rock".lower():
-        if computer_move == "Paper".lower():
+    elif player_move == "Rock":
+        if computer_move == "Paper":
             lose += 1
             print("-->You lose! Total loses:", lose)
         else:
             win += 1
             print("-->You win! Total wins:", win)
-    elif player_move == "Paper".lower():
+    elif player_move == "Paper":
         if computer_move == "Scissors".lower():
             lose += 1
             print("-->You lose! Total loses:", lose)
         else:
             win += 1
             print("-->You win!")
-    elif player_move == "Scissors".lower():
-        if computer_move == "Rock".lower():
+    elif player_move == "Scissors":
+        if computer_move == "Rock":
             lose += 1
             print("-->You lose! Total loses:", lose)
         else:
             win += 1
             print("--> You win! Total wins:", win)
-    elif player_move == "Q".lower():
+    elif player_move == "Q":
         playing = True
     else:
         print("-->That's not a valid play. Check your spelling")
